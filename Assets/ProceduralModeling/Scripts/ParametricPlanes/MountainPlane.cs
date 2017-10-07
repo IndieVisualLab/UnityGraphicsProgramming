e8eea1a8f34cdfee8a9d0e35cdae4ed19cf10af4
mattatz <supa.matz.naka@gmail.com>
2017-10-07 19:23:03 +0900

    Rename classes.

---
+++ Assets/ProceduralModeling/Scripts/ParametricPlanes/MountainPlane.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralModeling {
+
+	public class MountainPlane : ParametricPlaneBase {
+
+		// 山の高さの勾配の変化具合
+		[SerializeField, Range(0.5f, 3f)] protected float power = 1.25f;
+
+		protected override float Depth (float u, float v)
+		{
+			return Distance(u) * Distance(v) * depth;
+		}
+
+		protected float Distance(float v) {
+			return Mathf.Pow((0.5f - Mathf.Abs(0.5f - v)), power);
+		}
+
+	}
+		
+}
+
---
+++ Assets/ProceduralModeling/Scripts/ParametricPlanes/TerrainPlane.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralModeling {
+
+	public class TerrainPlane : ParametricPlaneBase {
+
+		[SerializeField, Range(0.1f, 10f)] protected float uScale = 1.2f, vScale = 1.5f;
+		[SerializeField] protected float uOffset = 0f, vOffset = 0f;
+
+		protected override float Depth (float u, float v)
+		{
+			return Mathf.PerlinNoise(u * uScale + uOffset, v * vScale + vOffset) * depth;
+		}
+
+	}
+		
+}
+
