ec65bb5a6c6f85b46afb4907adf3cc45fe34ffdd
fuqunaga <fukhid@gmail.com>
2017-10-10 22:57:11 +0900

    add RoomProjection

---
+++ Assets/RoomProjection/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace RoomProjection
+{
+    [RequireComponent(typeof(CameraSwitcher))]
+    public class Menu : MonoBehaviour
+    {
+
+        CameraSwitcher _cameraSwitcher;
+
+        void Start()
+        {
+            _cameraSwitcher = GetComponent<CameraSwitcher>();
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Move: WASD,↑←↓→");
+            GUILayout.Label("Jump: Space");
+
+            _cameraSwitcher.Menu();
+        }
+    }
+}
